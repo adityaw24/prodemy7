e75acc39038c85eeb7e7d78ab789b239ccce511b
Purwa Aditya Wisnu Wardhana <adityawisnu104@gmail.com>
2021-11-25 15:52:49 +0700

    edit function

--- javascript-star-pattern/star-pattern.js
+++ javascript-star-pattern/star-pattern.js
@@ -1,16 +1,18 @@
 const input = prompt('Enter number of star');
 
-console.log('star = ' + input);
-
 // function starPattern
 function starPattern(input) {
+
     let star = '';
     for (let i = 1; i <= input; i++) {
         for (let j = 1; j <= input; j++) {
-            star += '*';
+            star += '* ';
         }
         star += '\n';
     }
+
+    console.log('star = ' + input);
     console.log(star);
 }
+
 starPattern(input);
